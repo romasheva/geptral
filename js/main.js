fe91cad89fce4fb86a1c61ca4f8666dd239a90f2
Roman_Shevchenko <54140354+romasheva@users.noreply.github.com>
2020-08-19 23:37:37 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,9 @@ var canvas=document.getElementById('canv_line');
 var ctx = canvas.getContext('2d');
 
 function inRad(num) {
-	//я ведь говорил, что функция принимает угол в радианах?
 	return num * Math.PI / 180;
 }
-ctx.rotate(inRad(-90));
+ctx.rotate(inRad(-5));
 ctx.lineWidth = 2; // толщина линии
 
 ctx.arc(200,200,100,0, Math.PI,false);
