5eab7ef8a557375b3d9623c04a5b4eb678d3d57e
Roman_Shevchenko <54140354+romasheva@users.noreply.github.com>
2020-08-20 18:17:37 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-// var canvas=document.getElementById('canv_line');
-// var ctx = canvas.getContext('2d');
+var canvas=document.getElementById('canv_line');
+var ctx = canvas.getContext('2d');
 
-// function inRad(num) {
-// 	return num * Math.PI / 180;
-// }
-// ctx.rotate(inRad(-5));
-// ctx.lineWidth = 2; // толщина линии
+function inRad(num) {
+	return num * Math.PI / 180;
+}
+ctx.rotate(inRad(-5));
+ctx.lineWidth = 2; // толщина линии
 
-// ctx.arc(200,200,100,0, Math.PI,false);
-// ctx.stroke();
+ctx.arc(200,200,100,0, Math.PI,false);
+ctx.stroke();
 
