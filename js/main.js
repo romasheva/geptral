0967269ce33d18f124392bc835d684453f72f547
Roman_Shevchenko <54140354+romasheva@users.noreply.github.com>
2020-08-20 20:59:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// var hiddenBox = $( ".logo_ademet" );
-// $( "#bnt_logo" ).on( "click", function( event ) {
-//   hiddenBox.show(1);
-// });
+var hiddenBox = $( ".logo_ademet" );
+$( "#bnt_logo" ).on( "click", function( event ) {
+  hiddenBox.show(1);
+});
