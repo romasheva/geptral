68543f26261ae2c6e5efa5765912ac3e0a179ee5
Roman_Shevchenko <54140354+romasheva@users.noreply.github.com>
2020-08-21 21:04:40 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-// $(document).ready(function(){
-//     /* Beginning of Verbs */
-//         $("#back_line_canv").mouseenter(function(){
-//             $("#back_line_img").css("display", "inline");
-//         });
-//         $("#back_line_canv").mouseleave(function(){
-//             $("#back_line_img").css("display", "none");
-//         });
-//         // $("#back_line_canv").mouseenter(function(){
-//         //     $("#back_line_img").css("text-shadow", "1px 1px 1px red");
-//         // });
-//         // $("#back_line_canv").mouseleave(function(){
-//         // //     $("#back_line_img").css("text-shadow", "");
-//         // });
-//         /* Ending of Nouns */
-//     });
+$(document).ready(function(){
+    /* Beginning of Verbs */
+        $("#back_line_canv").mouseenter(function(){
+            $("#back_line_img").css("display", "inline");
+        });
+        $("#back_line_canv").mouseleave(function(){
+            $("#back_line_img").css("display", "none");
+        });
+        // $("#back_line_canv").mouseenter(function(){
+        //     $("#back_line_img").css("text-shadow", "1px 1px 1px red");
+        // });
+        // $("#back_line_canv").mouseleave(function(){
+        // //     $("#back_line_img").css("text-shadow", "");
+        // });
+        /* Ending of Nouns */
+    });
